7bde46f773d1a69aa8be18f8a943e2eafe5356ab
vijaykumarrpai <vijaykumarrpai@gmail.com>
2018-08-03 11:23:35 +0530

    creating a dummy db and accessing the data

--- products/app.js
+++ products/app.js
@@ -18,17 +18,50 @@ app.get('/', (req,res) => {
 // REST - Representational State Transfer Protocol
 // TO PERFORM AN OPERATION ON A RESOURCE / DATA YOU WILL NEED TO GO TO A END POINT WIHT A SPECIFIC HTTP METHOD
 
+// dummy db
+let products = [
+    {
+        id: 1,
+        name: 'Marker',
+        price: 15
+    },
+    {
+        id: 2,
+        name: 'Scale',
+        price: 5
+    },
+    {
+        id: 3,
+        name: 'Board',
+        price: 75
+    }
+];
+
 //READ
 // GET - /products
 // GET - /products/:id (shows a specific product)
 // app.METHOD(PATH, HANDLER)
 app.get('/products', (req,res) => {
     //write code to get the data from the database
-    res.send({
-        msg: 'get request made for /products'
-    });
+    // res.send({
+    //     msg: 'get request made for /products'
+    // });
+    res.send(products);
 });
 
+// GET - /products/:id (show a specific product)
+app.get('/products/:id', (req,res) => {
+    let product = products.find((product) => {
+        return product.id == req.params.id;
+    });
+    if(product) { // if product record is found
+        res.send(product);
+    } else { // if product record is not found
+        res.send({
+            notice: `product with id ${req.params.id} not found`
+        })
+    }
+});
 // CREATE
 // POST - /products
 // app.METHOD(PATH, HANDLER)
